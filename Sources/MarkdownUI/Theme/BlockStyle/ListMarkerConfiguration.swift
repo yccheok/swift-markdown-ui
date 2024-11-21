22bfeb4591d66ee6ef422e7b3fca3e5cf285df67
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2024-11-22 03:30:27 +0800

    1.5 -> 0.5

--- Sources/MarkdownUI/Theme/BlockStyle/ListMarkerConfiguration.swift
+++ Sources/MarkdownUI/Theme/BlockStyle/ListMarkerConfiguration.swift
@@ -15,37 +15,37 @@ public struct ListMarkerConfiguration {
 extension BlockStyle where Configuration == ListMarkerConfiguration {
   /// A list marker style that uses decimal numbers beginning with 1.
   public static var decimal: Self {
-    decimal(minWidth: .em(1.5), alignment: .trailing)
+    decimal(minWidth: .em(0.5), alignment: .trailing)
   }
 
   /// A list marker style that uses uppercase roman numerals beginning with `I`.
   public static var upperRoman: Self {
-    upperRoman(minWidth: .em(1.5), alignment: .trailing)
+    upperRoman(minWidth: .em(0.5), alignment: .trailing)
   }
 
   /// A list marker style that uses lowercase roman numerals beginning with `i`.
   public static var lowerRoman: Self {
-    lowerRoman(minWidth: .em(1.5), alignment: .trailing)
+    lowerRoman(minWidth: .em(0.5), alignment: .trailing)
   }
 
   /// A list marker style that uses a dash.
   public static var dash: Self {
-    dash(minWidth: .em(1.5), alignment: .trailing)
+    dash(minWidth: .em(0.5), alignment: .trailing)
   }
 
   /// A list marker style that uses a filled circle.
   public static var disc: Self {
-    disc(minWidth: .em(1.5), alignment: .trailing)
+    disc(minWidth: .em(0.5), alignment: .trailing)
   }
 
   /// A list marker style that uses a hollow circle.
   public static var circle: Self {
-    circle(minWidth: .em(1.5), alignment: .trailing)
+    circle(minWidth: .em(0.5), alignment: .trailing)
   }
 
   /// A list marker style that uses a filled square.
   public static var square: Self {
-    square(minWidth: .em(1.5), alignment: .trailing)
+    square(minWidth: .em(0.5), alignment: .trailing)
   }
 
   /// A list marker style that alternates between disc, circle, and square, depending on the list level.
